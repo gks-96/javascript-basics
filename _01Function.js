4171f21ed66a82ddd6c61914bd6311c1f3b429f4
gks-96 <gurkiratguliani@gmail.com>
2023-08-30 15:17:03 -0700

    js functions

---
+++ _01Function.js
@@ -0,0 +1,13 @@
+var name = "max";
+
+var age = 29
+
+var hasHobbies = true;
+console.log(name);
+
+
+function summarizeUser(username,userAge,userHasHobby){
+    return ("Name is " + username + "age is " + userAge + " userHasHobby  " + userHasHobby);
+}
+
+console.log(summarizeUser("max",29,true))
